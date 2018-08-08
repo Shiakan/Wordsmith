2fef1b779b19ad1c9e93c0a133bc35ed07a09f1e
Marion Job <marionjob@outlook.fr>
2018-08-08 22:54:18 +0200

    début CRUD article

---
+++ symfony/src/Controller/Backend/Codex/ArticleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller\Backend\Codex;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/backend/codex/article")
+ */
+class ArticleController extends Controller
+{
+    /**
+     * @Route("/", name="article_index", methods="GET")
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('backend/codex/article/index.html.twig', ['articles' => $articleRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="article_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('backend/codex/article/new.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_show", methods="GET")
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('backend/codex/article/show.html.twig', ['article' => $article]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="article_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('article_edit', ['id' => $article->getId()]);
+        }
+
+        return $this->render('backend/codex/article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+}
