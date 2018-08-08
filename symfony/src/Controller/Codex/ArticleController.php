f0654c594587dd58e3483592867f9d8120d9b79c
Mathieu BARRY <broly31820@gmail.com>
2018-08-08 19:18:49 +0200

    Ajout de la route via ID vers show sans affichage des informations des comments

--- symfony/src/Controller/Codex/ArticleController.php
+++ symfony/src/Controller/Codex/ArticleController.php
@@ -3,8 +3,9 @@
 namespace App\Controller\Codex;
 
 
-use App\Entity\Article;
 use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -29,8 +30,15 @@ class ArticleController extends Controller
     /**
      * @Route("/codex/show/{id}", name="show")
      */
-    public function show()
+    public function show(Article $article, Request $request, ObjectManager $manager)
     {
-        
+
+        $repository = $this->getDoctrine()->getRepository(Comment::class);
+        $comments = $repository->findAll();
+        return $this->render('article/show.html.twig', [
+            'article'=> $article,
+            'comments'=>$comments
+        ]);
     }
+    
 }
