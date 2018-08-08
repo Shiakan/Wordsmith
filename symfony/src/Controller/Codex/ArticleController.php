a36c3c4fcf107cd860fe06f7f84ed1c09b067021
Mathieu BARRY <broly31820@gmail.com>
2018-08-08 18:31:21 +0200

    Fonctionnalité liste Ok début de show

--- symfony/src/Controller/Codex/ArticleController.php
+++ symfony/src/Controller/Codex/ArticleController.php
@@ -4,22 +4,33 @@ namespace App\Controller\Codex;
 
 
 use App\Entity\Article;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController extends Controller
 {
     /**
-     * @Route("/codex/find", name="show")
+     * @Route("/codex/find", name="find")
      */
     public function find()
     {
         $repository= $this->getDoctrine()->getRepository(Article::class);
-        $articles = $repository->findAll();
+        $articles = $repository->findBy(array(),array("dateInserted"=>"desc"));
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
         ]);
     }
-
+    /**
+     * @Route("/codex/show/{id}", name="show")
+     */
+    public function show()
+    {
+        
+    }
 }
