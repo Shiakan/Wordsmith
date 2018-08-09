bc914292d0be9ee03b4aa23a7209dfff31203c66
Marion Job <marionjob@outlook.fr>
2018-08-09 12:30:29 +0200

    login form plus propre sans messages flash d'erreur

--- symfony/src/Form/LoginType.php
+++ symfony/src/Form/LoginType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class LoginType extends AbstractType
@@ -14,13 +15,9 @@ class LoginType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('_username', TextType::class, [
-                'label' => 'Nom d\'utilisateur'
-            ])
-            ->add('_password', PasswordType::class, [
-                'label' => 'Mot de passe'
-            ])
-        ;
+        ->add('username', TextType::class)
+        ->add('password', PasswordType::class)
+        ->add('logIn', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
