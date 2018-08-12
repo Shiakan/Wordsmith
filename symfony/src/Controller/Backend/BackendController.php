2366eb9bd09f66247c758a4e077120e463e12401
Mathieu BARRY <broly31820@gmail.com>
2018-08-12 21:58:42 +0200

    Add Backend Homepage

---
+++ symfony/src/Controller/Backend/BackendController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Backend;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BackendController extends Controller
+{
+    /**
+     * @Route("/backend", name="homebackend")
+     */
+    public function index()
+    {
+        return $this->render('backend/main/index.html.twig', [
+            'controller_name' => 'BackendController',
+        ]);
+    }
+}
