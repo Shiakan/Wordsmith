cf7bb5b2cb53573a4f634dc8fea881f8b244c76f
Marion Job <marionjob@outlook.fr>
2018-08-13 14:32:39 +0200

    hide comment function and button

---
+++ symfony/src/Controller/Backend/Codex/CommentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Backend\Codex;
+
+use App\Entity\Comment;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+/**
+ * @Route("/backend/codex/comment")
+ */
+
+class CommentController extends Controller
+{
+    /**
+     * @Route("/hide/{id}", name="hide_comment")
+     */
+    public function validate(Comment $comment)
+    {   
+        $em = $this->getDoctrine()->getManager();
+        // Si la réponse avait le statut false (donc 'non valide') alors on le passe à true = valide
+        if($comment->getStatus() === false) {
+            $comment->setStatus(true);
+            $em->flush();
+        }
+        else {
+            // Si la réponse avait le statut true (donc 'valide') on le passe à false = non valide
+            $comment->setStatus(false);
+            $em->flush();
+        }
+        // On redirige vers la page de la question concernée
+        return $this->redirectToRoute('article_show', ['id' => $comment->getArticle()->getId()]);
+    }
+}
