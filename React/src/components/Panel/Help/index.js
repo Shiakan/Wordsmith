4c3fcedc8b8c5190b5cb2d37e4e6043a7ddab0ff
rincewinderic <eric.naud@yahoo.fr>
2018-08-13 11:46:07 +0200

    help grid and overflow auto

--- React/src/components/Panel/Help/index.js
+++ React/src/components/Panel/Help/index.js
@@ -16,12 +16,17 @@ import './help.sass';
  */
 const Help = () => (
   <div className="help">
-    <p>Helptest</p>
-    <ol>
-      <li>ci</li>
-      <li>ca</li>
-      <li>co</li>
-      <li>ce</li>
+    <p className="help-title">Vous trouverez toute l'aide necessaire ici</p>
+    <ol className="help-ol">
+      <li className="help-ol-li">Pour communiquer avec les autres joueurs, vous pouvez utiliser le chat</li>
+      <li className="help-ol-li">Le Maitre du Jeu peut choisir de ne communiquer ou non le résultat de son lancer de dés</li>
+      <li className="help-ol-li">Les résultats des joueurs seront systématiquements reportés dans le chat-log</li>
+      <li className="help-ol-li">Pour communiquer avec les autres joueurs, vous pouvez utiliser le chat</li>
+      <li className="help-ol-li">Le Maitre du Jeu peut choisir de ne communiquer ou non le résultat de son lancer de dés</li>
+      <li className="help-ol-li">Les résultats des joueurs seront systématiquements reportés dans le chat-log</li>
+      <li className="help-ol-li">Pour communiquer avec les autres joueurs, vous pouvez utiliser le chat</li>
+      <li className="help-ol-li">Le Maitre du Jeu peut choisir de ne communiquer ou non le résultat de son lancer de dés</li>
+      <li className="help-ol-li">Les résultats des joueurs seront systématiquements reportés dans le chat-log</li>
     </ol>
   </div>
 );
