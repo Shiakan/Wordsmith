b246158d1f6d3a44e6b6d82ad46cdbb42cbbb54e
rincewinderic <eric.naud@yahoo.fr>
2018-08-13 19:57:04 +0200

    dice's still broken

--- React/src/store/reducers/dice.js
+++ React/src/store/reducers/dice.js
@@ -39,7 +39,7 @@ const roll = (dice) => {
 
   console.log('number of dices', numberOfDices);
   console.log('number of sides', numberOfSides);
-  if (typeof (numberOfSides) !== 'undefined' && numberOfSides > '0' && Number.isNaN(parseFloat(total))) {
+  if (typeof (numberOfSides) !== 'undefined' && numberOfSides > '0' && !Number.isNaN(parseFloat(total))) {
     for (let dices = 0; dices < numberOfDices; dices += 1) {
       total += Math.floor(Math.random() * numberOfSides) + 1;
       // console.log(total);
