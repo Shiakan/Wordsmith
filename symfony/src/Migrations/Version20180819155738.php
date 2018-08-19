5afc8ff97f994001f7aa4065e0cb78ca2d26de06
Mathieu BARRY <broly31820@gmail.com>
2018-08-19 18:32:18 +0200

    JumbotronCSS and Threads Records

--- symfony/src/Migrations/Version20180819155738.php
+++ symfony/src/Migrations/Version20180819155738.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180819110030 extends AbstractMigration
+final class Version20180819155738 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
