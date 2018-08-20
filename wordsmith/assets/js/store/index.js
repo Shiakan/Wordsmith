13975ac23d6dd1b3a7d7450b053dd655ce4b6714
mntn95 <mattngtn@gmail.com>
2018-08-20 17:27:20 +0200

    merge

--- wordsmith/assets/js/store/index.js
+++ wordsmith/assets/js/store/index.js
@@ -1,14 +1,15 @@
 /*
  * Npm import
  */
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 /*
  * Local import
  */
 // Reducer
-import reducers from '../store/reducers';
+import reducers from './reducers';
 
+import socket from './middlewares/socket';
 /*
  * Code
  */
@@ -17,8 +18,15 @@ if (window.devToolsExtension) {
   devTools.push(window.devToolsExtension());
 }
 
+const appliedMiddleware = applyMiddleware(socket);
+const enhancers = compose(appliedMiddleware, ...devTools);
+
 // createStore
-const store = createStore(reducers, ...devTools);
+const store = createStore(reducers, enhancers);
+
+
+// // createStore
+// const store = createStore(reducers, ...devTools);
 
 /*
  * Export
