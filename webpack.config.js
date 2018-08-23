15fffbad528fd5033743a67fce891ae2893e89f2
rincewinderic <eric.naud@yahoo.fr>
2018-08-23 18:04:17 +0200

    lint fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 Encore
-  // .configureRuntimeEnvironment('dev-server')
+  .configureRuntimeEnvironment('dev-server')
   // directory where compiled assets will be stored
   .setOutputPath('public/build/')
   // public path used by the web server to access the output path
