296496d856f700f804fa6ae4b4dbe51646fb1cd9
rincewinderic <eric.naud@yahoo.fr>
2018-08-28 11:09:56 +0200

    encore prof

--- assets/js/components/Logo/index.js
+++ assets/js/components/Logo/index.js
@@ -3,13 +3,13 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactTooltip from 'react-tooltip';
+// import ReactTooltip from 'react-tooltip';
 
 /**
  * Local import
  */
 // Composants
-
+import logo from '../../assets/img/logoaccident4.png';
 // Styles et assets
 import './logo.sass';
 
@@ -17,7 +17,7 @@ import './logo.sass';
  * Code
  */
 const Logo = ({ help, showHelp }) => (
-  <div className="logo">
+  <div className="logo" background={logo}>
     {help
         && (
         <div onClick={showHelp} className="help-over">
